f5985bdbcfb54bb0b17c796eeec8c6294920289e
rafaeelricco <rafaelricco10@gmail.com>
2024-03-23 10:28:39 -0300

    fix: add Google Tag Manager script to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@
 import { MantineProvider } from '@mantine/core'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 import NProgress from 'nprogress'
 import React, { useEffect } from 'react'
 import TagManager from 'react-gtm-module'
@@ -74,6 +75,17 @@ export default function App(props: Props) {
       <MantineProvider>
         <Component {...pageProps} />
       </MantineProvider>
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KBTCMFPM');`
+        }}
+      />
     </>
   )
 }
